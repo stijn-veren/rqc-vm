62cd2f595783ee331e5a5c27dabc1199cb0eccb8
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-23 11:17:59 +0300

    Add property and pass it to child component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import TodoList from './Todo/TodoList';
 
 function App() {
+  const todos = [
+    { id: 1, completed: false, title: 'Buy butter' },
+    { id: 2, completed: false, title: 'Buy bread' },
+    { id: 3, completed: false, title: 'Buy meat' },
+  ];
+
   return (
     <div className="wrapper">
       <h1>Todo List</h1>
 
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 }
