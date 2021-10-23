df3204231bc9619fa26a7b1f00ea8c9fec99d52e
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-23 14:55:18 +0300

    Add state, hook and a function to update it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import TodoList from './Todo/TodoList';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: 'Buy butter' },
     { id: 2, completed: false, title: 'Buy bread' },
     { id: 3, completed: false, title: 'Buy meat' },
-  ];
+  ]);
 
   function toggleTodo(id) {
-    console.log('todo id', id);
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
   }
 
   return (
