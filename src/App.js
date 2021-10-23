46e5b3517d8109ad118ba35435b967bb56b5b5a6
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-23 17:25:33 +0300

    Add modal as classic component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context';
 import Loader from './Loader';
+import Modal from './Modal/Modal';
 
 const AddTodo = React.lazy(
   () =>
@@ -60,6 +61,8 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>Todo List</h1>
+        <Modal />
+
         <React.Suspense fallback={<Loader />}>
           <AddTodo onCreate={addTodo} />
         </React.Suspense>
